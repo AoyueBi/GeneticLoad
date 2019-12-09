4d329df0c560d966a7b1e90166c522e73d9b1fb6
AoyueBi <aoyuebi@gmail.com>
2019-12-09 18:15:37 +0800

    tree preparation

---
+++ src/AoUtils/AoFile.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AoUtils;
+
+import java.io.File;
+
+/**
+ *
+ * @author Aoyue
+ */
+public class AoFile {
+    public AoFile(){
+        
+    }
+    
+    public void copyFileDirS(String infileDirS, String outfileDirS){
+//        String infileDirS = "/Users/Aoyue/project/wheatVMapII/003_dataAnalysis/005_vcf/018_annoDB/103_snpClassify/002_changeChrPos";
+//        String outfileDirS = "/Users/Aoyue/project/wheatVMapII/003_dataAnalysis/005_vcf/018_annoDB/103_snpClassify/003_mergebySub";
+        
+        
+        File[] fs = new File(infileDirS).listFiles();
+        for (int i = 0; i < fs.length; i++) {
+            if (fs[i].isHidden()) {
+                fs[i].delete();
+            }
+        }
+        fs = new File(infileDirS).listFiles();
+        for (int i = 0; i < fs.length; i++) {
+            System.out.println(fs[i]);
+            new File(outfileDirS, fs[i].getName()).mkdirs();
+        }
+    }
+}
---
+++ src/genomeSize/operators.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package genomeSize;
+
+/**
+ *
+ * @author mingzhang
+ */
+public class operators {
+    public static void main (String[] args){
+        
+        int result = 1+2;
+        System.out.println(result);
+        
+        result = result -1 ;
+        System.out.println(result);
+        
+        
+        
+        int month = 2;
+        String monthString = null;
+        switch (month){
+            case 1: monthString = "1";
+                    break;
+            case 2: monthString = "2";
+            break;
+        }
+        System.out.println(monthString);
+        
+        
+    }
+    
+        }
+
+
+        
+    
+    
+
