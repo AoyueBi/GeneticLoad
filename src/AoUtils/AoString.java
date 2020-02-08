bfb5cfa7613159da9c61a0546878464b1f022873
AoyueBi <aoyuebi@gmail.com>
2020-02-09 00:10:58 +0800

    Pi mean

--- src/AoUtils/AoString.java
+++ src/AoUtils/AoString.java
@@ -4,7 +4,7 @@ public class AoString {
     public AoString(){
 
     }
-
-    //                                String chr = name.substring(name.indexOf("chr")+3,name.indexOf("chr")+5);
-    //                                String pop1pop2 = name.substring(0,name.indexOf("_chr"));
+//String name = new File(outPath).getName();
+    //String chr = name.substring(name.indexOf("chr")+3,name.indexOf("chr")+5);
+    //String pop1pop2 = name.substring(0,name.indexOf("_chr"));
 }
--- src/PopulationAnalysis/Pi.java
+++ src/PopulationAnalysis/Pi.java
@@ -1,16 +1,94 @@
 package PopulationAnalysis;
 
-import AoUtils.SplitScript;
+import AoUtils.AoFile;
+import gnu.trove.list.array.TDoubleArrayList;
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import pgl.utils.IOUtils;
+import pgl.utils.PStringUtils;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Pi {
     public Pi(){
 //        this.mkPiCommandbasedwinndow();
-        new SplitScript().splitScript2("/Users/Aoyue/project/wheatVMapII/003_dataAnalysis/005_vcf/019_popGen/102_Pi/001_script_based2Mwindow_1Mstep/pi_based2Mwindow_1Mstep_20200207.sh",23,4); //91cmd
+//        new SplitScript().splitScript2("/Users/Aoyue/project/wheatVMapII/003_dataAnalysis/005_vcf/019_popGen/102_Pi/001_script_based2Mwindow_1Mstep/pi_based2Mwindow_1Mstep_20200207.sh",23,4); //91cmd
 
+        this.extractVCFlog();
+    }
+
+
+    /**
+     * get mean from a file， is the value equals -nan, continue
+     *
+     * @param infileS
+     * @return
+     */
+    public String getMean(String infileS){
+        String out = null;
+        try {
+            BufferedReader br = IOUtils.getTextReader(infileS);
+            String temp = br.readLine();
+            List<String> l = new ArrayList<>();
+            TDoubleArrayList vList = new TDoubleArrayList();
+            int cnt = 0;
+            while ((temp = br.readLine()) != null) {
+                l = PStringUtils.fastSplit(temp);
+                String t = l.get(4);
+                if (t.startsWith("-n") || t.startsWith("n")) { //pos中 fst值为 -nan 的全部去掉
+                    continue;
+                }
+                double v = Double.parseDouble(t);
+                vList.add(v);
+                cnt++; //有 fst值的位点数目
+            }
+            br.close();
+            double[] v = vList.toArray();
+            DescriptiveStatistics d = new DescriptiveStatistics(v);
+            double m = d.getMean();
+            out = String.format("%.6f", m);
+            System.out.println(infileS + " with mean pi " + out + " is completed");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return out;
+    }
+
+    public void extractVCFlog(){
+        String taxaList = "/Users/Aoyue/project/wheatVMapII/003_dataAnalysis/005_vcf/001_taxaList/002_groupbyPloidy_removeBadTaxa/taxaList.txt";
+        HashMap<String,String> hm = new AoFile().getHashMap(taxaList,11,8);
+
+        String infileDirS = "/Users/Aoyue/project/wheatVMapII/003_dataAnalysis/005_vcf/019_popGen/102_Pi/002_pi_based2Mwindow_1Mstep";
+        String outfileS = "/Users/Aoyue/project/wheatVMapII/003_dataAnalysis/005_vcf/019_popGen/102_Pi/003_meadPI/Pi_bySubspecies_20200208.txt";
+        List<File> fsList = IOUtils.getVisibleFileListInDir(infileDirS);
+        try {
+            BufferedWriter bw = IOUtils.getTextWriter(outfileS);
+            bw.write("CHROM\tSub\tGroup\tPloidy\tMEAN_PI");
+            bw.newLine();
+            for (int i = 0; i < fsList.size(); i++) {
+                String infileS = fsList.get(i).getAbsolutePath();
+                String name = new File(infileS).getName(); //Cultivar_chr1D_based2000000Window_1000000step.windowed.pi
+                String chr = name.substring(name.indexOf("chr")+3,name.indexOf("chr")+5);
+                String sub = chr.substring(1);
+                String group = name.substring(0,name.indexOf("_chr"));
+                String ploidy = hm.get(group);
+                String value = this.getMean(infileS);
+                bw.write(chr + "\t" + sub + "\t" + group + "\t" + ploidy + "\t" + value);
+                bw.newLine();
+
+            }
+            bw.flush();
+            bw.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 
     public void mkPiCommandbasedwinndow(){
